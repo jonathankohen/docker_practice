1e453758cb8585348bb3ba8b70064535a5e9801a
Jonathan Kohen <jonathan.a.kohen@gmail.com>
2021-07-06 15:45:09 -0400

    added gitignore

--- dock/settings.py
+++ dock/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "lo_tc@i%vo=+6_^#klf3rodt3x45!5e=d893=609rl*!t6jx$5"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
